3e42cd0f941744ecde2245e0face41257e21a3ec
Tingting Ji Zhou <168580008+TingtingJiZ@users.noreply.github.com>
2024-07-27 08:10:19 +0000

    pep8

--- src/api/routes.py
+++ src/api/routes.py
@@ -15,4 +15,37 @@ CORS(api)  # Allow CORS requests to this API
 def handle_hello():
     response_body = {}
     response_body["message"] = "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    return jsonify(response_body), 200
+    return response_body, 200
+
+@api.route("/users", methods=["GET", "POST"])
+def handle_users():
+    response_body = {}
+    if request.method == "GET":
+        rows = db.session.execute(db.select(Users)).scalars()
+        results = [row.serialize() for row in rows]  # List comprehesion
+        response_body["results"] = results
+        response_body["message"] = "GET request"
+        return response_body, 200
+    if request.method == "POST":
+        response_body["message"] = "POST request"
+        return response_body, 200
+
+
+@api.route("/users/<int:user_id>", methods=["GET", "PUT", "DELETE"])
+def handle_user(user_id):
+    response_body = {}
+    if request.method == "GET":
+        row = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+        if not row:
+              response_body["results"] = {}
+              response_body["message"] = f'doesnt exist {user_id}'
+              return response_body, 404
+        response_body["results"] = row.serialize()
+        response_body["message"] = f'Recibi el GET request {user_id}'
+        return response_body, 200
+    if request.method == "PUT":
+        response_body["message"] = f'Recibi el PUT request {user_id}'
+        return response_body, 200
+    if request.method == "DELETE":
+        response_body["message"] = f'Recibi el DELETE request {user_id}'
+        return response_body, 200
