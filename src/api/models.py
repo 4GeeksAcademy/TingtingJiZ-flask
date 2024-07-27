bc307b2931892b9161d594c00f586a4f228f0e2b
Tingting Ji Zhou <168580008+TingtingJiZ@users.noreply.github.com>
2024-07-27 09:43:52 +0000

    model

--- src/api/models.py
+++ src/api/models.py
@@ -5,11 +5,14 @@ db = SQLAlchemy()
 
 
 class Users(db.Model):
+    __tablename__ = "users"
+    # Atributos
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     is_admin = db.Column(db.Boolean(), unique=False, nullable=False)
+    # Relaciones
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -20,3 +23,37 @@ class Users(db.Model):
                 "email": self.email,
                 "is_active": self.is_active,
                 "is_admin": self.is_admin}
+
+class Authors(db.Model):
+    # __tablename__ = "authors" this way only if is not a single words
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=False, nullable=False)
+    lastname = db.Column(db.String, unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    # Relaciones
+    user_to = db.relationship("Users", foreign_keys=[user_id],
+                               backref=db.backref("author_to", lazy="select"))
+
+    def __repr__(self):
+        return f"<Author: {self.id} - {self.name}>"
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name,
+                "lastname": self.lastname,
+                "user_id": self.user_id}
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    author_to = db.relationship('Authors', foreign_keys=[author_id], 
+                                 backref=db.backref('book_to', lazy='select'))
+    
+    def __repr__(self):
+        return f"<Book: {self.id} - {self.tittle}>"
+    
+    def serialize(self):
+        return {"id": self.id,
+                "title": self.title,
+                "author_id": self.author_id}
