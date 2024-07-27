18b70309c8a26931fa67e9ae6bed2a573d044ca3
Tingting Ji Zhou <168580008+TingtingJiZ@users.noreply.github.com>
2024-07-27 18:42:07 +0000

    cards border fixed

--- src/front/js/pages/SpeciesDetails.jsx
+++ src/front/js/pages/SpeciesDetails.jsx
@@ -9,28 +9,19 @@ export const SpeciesDetails = () => {
         <div className="container bg-dark mb-3 mt-5">
             <h1 className="text-center text-light mt-3">Details</h1>
             <div className="row row-cols-1 row-cols-md-3 row-cols-xl-5 g-2 justify-content-center">
-                <div className="card mx-2 my-3">
-                    <img className="card-img-top mt-0" src={`https://starwars-visualguide.com/assets/img/species/${store.currentSpecies.uid}.jpg`} alt="species" />
-                    <div className="card-body">
-                        <h5 className="card-title">Classification: {item.classification}</h5>
-                        <p className="card-text "> <strong>Designation: </strong>{item.designation}</p>
-                        <p className="card-text "><strong>Average lifespan: </strong>{item.average_lifespan}</p>
-                        <p className="card-text "><strong>Average height: </strong>{item.average_height}</p>
-                        <p className="card-text "> <strong>Lenguage: </strong>{item.lenguage}</p>
+                <div style={{ width: "18rem" }}>
+                    <div className="card mx-2 my-3">
+                        <img className="card-img-top mt-0" src={`https://starwars-visualguide.com/assets/img/species/${store.currentSpecies.uid}.jpg`} alt="species" />
+                        <div className="card-body">
+                            <h5 className="card-title">Classification: {item.classification}</h5>
+                            <p className="card-text "> <strong>Designation: </strong>{item.designation}</p>
+                            <p className="card-text "><strong>Average lifespan: </strong>{item.average_lifespan}</p>
+                            <p className="card-text "><strong>Average height: </strong>{item.average_height}</p>
+                            <p className="card-text "> <strong>Lenguage: </strong>{item.lenguage}</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     )
 }
-
-/* "classification": "mammal",
-            "designation": "sentient",
-            "average_height": "180",
-            "average_lifespan": "120",
-            "hair_colors": "blonde, brown, black, red",
-            "skin_colors": "caucasian, black, asian, hispanic",
-            "eye_colors": "brown, blue, green, hazel, grey, amber",
-            "homeworld": "https://www.swapi.tech/api/planets/1",
-            "language": "Galactic Basic",
-            "people" */
