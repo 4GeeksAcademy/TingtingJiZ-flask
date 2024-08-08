64107091c9aad070d32e993bcb1aa156bd14dc52
Tingting Ji Zhou <168580008+TingtingJiZ@users.noreply.github.com>
2024-08-08 18:56:27 +0000

    some fixs

--- src/front/js/component/Logout.jsx
+++ src/front/js/component/Logout.jsx
@@ -1,14 +1,18 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
+
 
 export const Logout = () => {
     const { actions } = useContext(Context)
+    const navigate = useNavigate()
 
     const handleLogout = () => {
         localStorage.clear()
         actions.setIsLogged(false);
         actions.setCurrentUser(null);
         actions.setAlert({visible: false, back: "info", text: ""})
+        navigate("/")
     }
 
     return (
--- src/front/js/pages/PlanetsDetails.jsx
+++ src/front/js/pages/PlanetsDetails.jsx
@@ -17,14 +17,16 @@ export const PlanetsDetails = () => {
         <div className="container bg-dark mb-3 mt-5">
             <h1 className="text-center text-light mt-3">Details</h1>
             <div className="row row-cols-1 row-cols-md-3 row-cols-xl-5 g-2 justify-content-center">
-                <div className="card mx-2 my-3">
-                    <img className="card-img-top mt-0" src={`https://starwars-visualguide.com/assets/img/planets/${store.currentPlanet.uid}.jpg`} alt="planets" />
-                    <div className="card-body">
-                        <h5 className="card-title">Name: {item.name}</h5>
-                        <p className="card-text "> <strong>Climate: </strong>{item.climate}</p>
-                        <p className="card-text "><strong>Diameter: </strong>{item.diameter}</p>
-                        <p className="card-text "><strong>Gravity: </strong>{item.gravity}</p>
-                        <p className="card-text "> <strong>Terrain: </strong>{item.terrain}</p>
+                <div style={{ width: "18rem" }}>
+                    <div className="card mx-2 my-3">
+                        <img className="card-img-top mt-0" src={`https://starwars-visualguide.com/assets/img/planets/${store.currentPlanet.uid}.jpg`} alt="planets" />
+                        <div className="card-body">
+                            <h5 className="card-title">Name: {item.name}</h5>
+                            <p className="card-text "> <strong>Climate: </strong>{item.climate}</p>
+                            <p className="card-text "><strong>Diameter: </strong>{item.diameter}</p>
+                            <p className="card-text "><strong>Gravity: </strong>{item.gravity}</p>
+                            <p className="card-text "> <strong>Terrain: </strong>{item.terrain}</p>
+                        </div>
                     </div>
                 </div>
             </div>
